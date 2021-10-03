8e598daca19b914dbf3e55ca7da9aceb78d01e47
Christian Przybulinski <christian.przybulinski@gmail.com>
2021-10-03 03:20:09 -0300

    creating default errors

---
+++ src/errors/errors.go
@@ -0,0 +1,63 @@
+package errors
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+var (
+	ErrInternal = &Error{
+		Code:    http.StatusInternalServerError,
+		Message: "Internal Server Error (500)",
+	}
+	ErrBadRequest = &Error{
+		Code:    http.StatusBadRequest,
+		Message: "Bad Request (400)",
+	}
+	ErrNotFound = &Error{
+		Code:    http.StatusNotFound,
+		Message: "Not Found (404)",
+	}
+)
+
+type Error struct {
+	Code    int
+	Message string
+}
+
+func GetError(e error) *Error {
+	res, ok := e.(*Error)
+	if !ok {
+		return ErrInternal
+	} else {
+		return res
+	}
+}
+
+func (err *Error) Error() string {
+	return err.String()
+}
+
+func (err *Error) String() string {
+	if err == nil {
+		return ""
+	}
+	return fmt.Sprintf("error: code=%s message=%s", http.StatusText(err.Code), err.Message)
+}
+
+// Converter para JSON
+func (err *Error) JSON() string {
+	if err == nil {
+		return ("{}")
+	}
+	res, _ := json.Marshal(err)
+	return string(res)
+}
+
+func (err *Error) StatusCode() int {
+	if err == nil {
+		return http.StatusOK
+	}
+	return err.Code
+}
